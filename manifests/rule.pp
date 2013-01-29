4428f6fbf841cc7dbf63492c2d56c8a10c628c1f
jerger <dev@jerger.rog>
2013-01-29 15:17:32 +0100

    fixed bug - fragment name now is unique

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -107,7 +107,7 @@ define iptables::rule (
     notify  => Service['iptables'],
   }
   
-  concat::fragment{ "iptables_rule_$name":
+  concat::fragment{ "iptables_rule_$name_v6":
     target  => $iptables::config_file_v6,
     content => template('iptables/concat/rule.erb'),
 #    content => "${command} ${chain} ${true_rule} -j ${target}\n",
