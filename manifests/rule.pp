5248a1b0c0e271c6815fff4f47112f8e67039ce8
jerger <dev@jerger.rog>
2013-01-29 16:15:48 +0100

    v4 adresses will not work in ipv6 table

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -107,13 +107,14 @@ define iptables::rule (
     notify  => Service['iptables'],
   }
   
-  concat::fragment{ "iptables_rule_v6_$name":
-    target  => $iptables::config_file_v6,
-    content => template('iptables/concat/rule.erb'),
-#    content => "${command} ${chain} ${true_rule} -j ${target}\n",
-    order   => $true_order,
-    ensure  => $ensure,
-    notify  => Service['iptables'],
+  if array_source.size <= 0 && array_destination <= 0 { 
+    concat::fragment{ "iptables_rule_v6_$name":
+      target  => $iptables::config_file_v6,
+      content => template('iptables/concat/rule.erb'),
+  #    content => "${command} ${chain} ${true_rule} -j ${target}\n",
+      order   => $true_order,
+      ensure  => $ensure,
+      notify  => Service['iptables'],
+    }
   }
-
 }
