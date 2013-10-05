151eda287a3b008019837e15cafead3635418052
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2013-10-06 00:37:19 +0200

    Cleaning up

--- lib/puppet/parser/functions/iptables_construct_implicit_matches.rb
+++ lib/puppet/parser/functions/iptables_construct_implicit_matches.rb
@@ -4,7 +4,6 @@ module Puppet::Parser::Functions
 Construct the Implicit Matches hash map for the Iptables module
     EOS
   ) do |arguments|
-  
 
     raise(Puppet::ParseError, "iptables_construct_implicit_matches(): Wrong number of arguments " +
       "given (#{arguments.size} for 1)") if arguments.size < 2
@@ -16,7 +15,6 @@ Construct the Implicit Matches hash map for the Iptables module
     active_version   = is_ipv6 ? '6' : '4'
     unactive_version = is_ipv6 ? '4' : '6'
     implicit_matches_str = ""
-#  puts implicit_matches.inpsect
 
     implicit_matches.each do |k, v|
       if k == 'invert' or k[-3, 3] == "_v#{unactive_version}" or ! implicit_matches["#{k}_v#{active_version}"].nil? or k[-3, 3] == '_v#{unactive_version}'
