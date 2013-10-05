f5c3078f9a765f4045e4af6939ab51931b972324
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2013-10-05 22:53:13 +0200

    Removed all .each() calls in the rulesets

--- manifests/ruleset/multicast.pp
+++ manifests/ruleset/multicast.pp
@@ -9,19 +9,18 @@ class iptables::ruleset::multicast (
   $log_level       = $iptables::log_level,
 ) {
 
-  each($chains) |$chain| {
-    iptables::rule { "example42-multicast-filter-${chain}":
-      table            => 'filter',
-      chain            => $chain,
-      explicit_matches => { 'pkttype' => {'pkt-type' => 'multicast'}},
-      target           => $target,
-      order            => $order,
-      log              => $log,
-      log_prefix       => $log_prefix,
-      log_limit_burst  => $log_limit_burst,
-      log_limit        => $log_limit,
-      log_level        => $log_level
-    }
-  }
+  $discard = iptables_declare_multiple('iptables::rule', $chains,
+                                             'example42-multicast-filter-###name###', {
+    table            => 'filter',
+    chain            => '###name###',
+    explicit_matches => { 'pkttype' => {'pkt-type' => 'multicast'}},
+    target           => $target,
+    order            => $order,
+    log              => $log,
+    log_prefix       => $log_prefix,
+    log_limit_burst  => $log_limit_burst,
+    log_limit        => $log_limit,
+    log_level        => $log_level
+  })
 
 }
