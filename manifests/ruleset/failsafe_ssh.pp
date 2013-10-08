3c07b008e6c25bc35d67e8b7770848e7fdfc568f
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2013-10-08 19:54:23 +0200

    Failsafe_ssh: Lookup a potentially alternative port as well

--- manifests/ruleset/failsafe_ssh.pp
+++ manifests/ruleset/failsafe_ssh.pp
@@ -1,14 +1,15 @@
 
 class iptables::ruleset::failsafe_ssh (
-  $chains          = [ 'INPUT', 'OUTPUT' ],
-  $target          = 'ACCEPT',
-  $order           = 11,
-  $port            = 22,
-  $log             = false,
-  $log_prefix      = $iptables::log_prefix,
-  $log_limit_burst = $iptables::log_limit_burst,
-  $log_limit       = $iptables::log_limit,
-  $log_level       = $iptables::log_level,
+  $chains                  = [ 'INPUT', 'OUTPUT' ],
+  $target                  = 'ACCEPT',
+  $order                   = 11,
+  $port                    = 22,
+  $log                     = false,
+  $lookup_alternative_port = true,
+  $log_prefix              = $iptables::log_prefix,
+  $log_limit_burst         = $iptables::log_limit_burst,
+  $log_limit               = $iptables::log_limit,
+  $log_level               = $iptables::log_level,
 ) {
   
   $discard = iptables_declare_multiple('iptables::rule',
@@ -26,5 +27,28 @@ class iptables::ruleset::failsafe_ssh (
     log_limit       => $log_limit,
     log_level       => $log_level
   })
+
+  # If openssh has been configured to use a different class we'll
+  # usee that too.
+  # We could combine the two rule statements into one by using a
+  # multiport match, but not all kernels support this, so for a
+  # failsafe we're not taking any chances.
+  if $lookup_alternative_port and defined(Class['openssh']) and $openssh::port != $port {
+      $discard_1 = iptables_declare_multiple('iptables::rule',
+        $chains, 'example42-failsafe-ssh-###name###-otherPort',
+    {
+      table           => 'filter',
+      chain           => '###name###',
+      target          => $target,
+      protocol        => 'tcp',
+      port            => $openssh::port,
+      order           => $order,
+      log             => $log,
+      log_prefix      => $log_prefix,
+      log_limit_burst => $log_limit_burst,
+      log_limit       => $log_limit,
+      log_level       => $log_level
+    })
+  }
 
 }
