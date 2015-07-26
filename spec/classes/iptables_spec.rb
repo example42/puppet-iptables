55e793f72580358a59c6eaf0654b8f6ea02bbd70
Martin Alfke <ma@example42.com>
2015-07-26 21:52:16 +0200

    Proper Projekt URL & Remove Puppet 2.7 tests
    - puppetlabs concat does no longer work with puppet 2
    - add ruby 2.1.1 tests

--- spec/classes/iptables_spec.rb
+++ spec/classes/iptables_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 
 describe 'iptables' do
   let(:node) { 'iptables1.example42.com' }
-  let(:facts) { { :operatingsystem => 'ubuntu', :osver_maj => 12, :concat_basedir => '/tmp'  } }
+  let(:facts) { {
+      :operatingsystem => 'ubuntu',
+      :osver_maj => 12,
+      :concat_basedir => '/tmp',
+      :operatingsystemrelease => '12.10'
+  } }
     
   it { should contain_iptables__concat_emitter('v4') }
   it { should_not contain_iptables__concat_emitter('v6') }
@@ -11,7 +16,12 @@ end
 
 describe 'iptables' do
   let(:node) { 'iptables2.example42.com' }
-  let(:facts) { { :operatingsystem => 'ubuntu', :osver_maj => 12, :concat_basedir => '/tmp'  } }
+  let(:facts) { {
+      :operatingsystem => 'ubuntu',
+      :osver_maj => 12,
+      :concat_basedir => '/tmp',
+      :operatingsystemrelease => '12.10'
+  } }
   let(:params) { { :enable_v6 => 'true' } }
   
   it { should contain_iptables__concat_emitter('v4') }
